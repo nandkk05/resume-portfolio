9a26fe7b74bb396dca9a6300762a2b5e059f29e4
Nand Kishor <nandkk07@gmail.com>
2020-05-25 00:40:38 +0530

    first version of react portfolio

---
+++ src/components/Left.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+
+function Left(){
+    return(
+        <div className="left">
+            <div className="bg-img"></div>
+            <div className="left-inner">
+              <div className="info">
+                <h2 className="name">Nand</h2>
+                <div className="subtext">Old School develuper</div>
+                <div className="jobStatus">Chandigarh Group of Colleges</div>
+                <div className="social-links">
+                  <a href="{this.props.twitterURL}" target="_blank"><i className="fa fa-twitter"></i></a>
+                  <a href="{this.props.instaURL}" target="_blank"><i className="fa fa-instagram"></i></a>
+                  <a href="{this.props.githubURL}" target="_blank"><i className="fa fa-github"></i></a>
+                  <a href="{this.props.linkedinURL}"  target="_blank"><i className="fa fa-linkedin"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+    )
+}
+
+export default Left;
