091288c12fe67c3a1ce2b04cdd54a484eea3ce66
Nand Kishor <nandkk07@gmail.com>
2021-04-21 15:40:42 +0530

    wide screen responsive

--- src/components/Left.js
+++ src/components/Left.js
@@ -9,7 +9,7 @@ function Left(){
               <div className="info">
                 <h2 className="name">Nand Kishor</h2>
                 <div className="subtext">Frontend Developer</div>
-                <div className="college">Chandigarh Group of Colleges</div>
+                <div className="left-college">Chandigarh Group of Colleges</div>
                 <div className="social-links">
                   <a href="https://twitter.com/nandkk05" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter"></i></a>
                   <a href="https://instagram.com/nandkk05" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram"></i></a>
