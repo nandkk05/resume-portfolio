58e3e8f966f7cc2eeb16cf24b9de55a3fbfec296
Nand Kishor <nandkk07@gmail.com>
2021-04-21 16:21:36 +0530

    project updated

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,7 +10,7 @@ export default function Projects() {
                 
                   <div className="project-item">
                     <a className="project-title" href="http://gadacts.com/" target="_blank" rel="noopener noreferrer">Gadacts.com</a>
-                    <p className="project-desc">I'm recently working on a platform where you can get all latest tech related stuff, instant news and tech hacks and this is named as "Gadacts."</p>
+                    <p className="project-desc">I'm recently working on a platform where you can get all latest tech related stuff, instant news and tech hacks and this is named as Gadacts.</p>
                   </div>
                   <div className="project-item">
                     <a className="project-title" href="https://github.com/nandkk05/kripto" target="_blank" rel="noopener noreferrer">Kripto</a>
@@ -19,6 +19,10 @@ export default function Projects() {
                   <div className="project-item">
                     <a className="project-title" href="https://github.com/nandkk05/meet-proxy" target="_blank" rel="noopener noreferrer">Meet Proxy</a>
                     <p className="project-desc">Python script to automatically join the Google Classroom. We can run this script on any VM and have no 75% attendance issue</p>
+                  </div>
+                  <div className="project-item">
+                    <a className="project-title" href="https://github.com/nandkk05/telegram-build-notifier.git" target="_blank" rel="noopener noreferrer">Telegram build Notifier</a>
+                    <p className="project-desc">Stay updated with CI build status on Telegram. Github action to deliver you the messages on telegram about every successful or failed build</p>
                   </div>
                   <div className="project-item">
                     <a className="project-title" href="https://github.com/nandkk05/superchat-web-app" target="_blank" rel="noopener noreferrer">SuperChat App</a>
